ef4627a433b4a7f293e575ed6848ce13679b05c4
AyaMu <ayalamuruch@gmail.com>
2023-02-23 23:14:03 -0500

    update migration for reaction to match reaction model

--- migrations/20230224035720-create-reaction.js
+++ migrations/20230224035720-create-reaction.js
@@ -10,16 +10,33 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       emoji: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        validate: {
+          min: 0,
+          max: 5
+        },
       },
-      imageId: {
-        type: Sequelize.INTEGER
+      postId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
       profileId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Profiles',
+          key: 'id'
+        },
       },
       reactorId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Profiles',
+          key: 'id'
+        }
       },
       createdAt: {
         allowNull: false,
