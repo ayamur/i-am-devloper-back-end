6269d44cbeacabd80129d4581b74145a002242af
AyaMu <ayalamuruch@gmail.com>
2023-02-25 17:57:52 -0500

    add deleteAccount to auth controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -11,6 +11,16 @@ async function createPost(req, res) {
   }
 }
 
+async function createPost(req, res) {
+  try {
+    const post = await Post.create(req.body)
+    res.status(200).json(post)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ err: error })
+  }
+}
+
 
 module.exports = {
   createPost
