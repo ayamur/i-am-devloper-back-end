3decf7212aa4f990268308cd6072e7f4c9396269
AyaMu <ayalamuruch@gmail.com>
2023-02-27 14:30:30 -0500

    update fly.toml

--- controllers/posts.js
+++ controllers/posts.js
@@ -20,7 +20,7 @@ async function getPosts(req, res) {
     const posts = await Post.findAll()
     res.status(200).json(posts)
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json({error: error.message})
   }
 }
 
